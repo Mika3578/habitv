e0d377406037aca325d861a206e73c97e72d58f0
Mika3578 <58137747+Mika3578@users.noreply.github.com>
2025-06-15 08:52:36 +0200

    refactor: merge gui into trayView and reorganize application structure

---
+++ application/trayView/src/com/dabi/habitv/tray/view/CredentialPanel.java
@@ -0,0 +1,14 @@
+package com.dabi.habitv.tray.view;
+
+import com.dabi.habitv.core.config.CredentialConfig;
+import com.dabi.habitv.core.service.CredentialManager;
+import org.apache.log4j.Logger;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+// ... rest of the file unchanged ... 
